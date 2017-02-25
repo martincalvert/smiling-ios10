ff1d7861737fa8d105661c4fbd0be2902d9d38a2
calvm022 <martin.calvert@disney.com>
2017-02-25 01:02:04 -0800

    Initial Commit

---
+++ faceView-ios10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faceView-ios10
+//
+//  Created by Calvert, Martin on 2/25/17.
+//  Copyright © 2017 Calvert, Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
